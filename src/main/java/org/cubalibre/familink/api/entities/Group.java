5e9ea504872ddb08b26fe2a679d9f00fc89258ff
souf59 <souf.el-atmani@hotmail.fr>
2017-10-11 12:45:17 +0200

    Add Group_Annotation

--- src/main/java/org/cubalibre/familink/api/entities/Group.java
+++ src/main/java/org/cubalibre/familink/api/entities/Group.java
@@ -3,17 +3,26 @@ package org.cubalibre.familink.api.entities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+@Entity
+@Table(name = "group")
 
 public class Group {
     private static final Logger LOG = LoggerFactory.getLogger(Group.class.getName());
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_group")
     private Integer id;
+    @Column(name = "owner")
     private User owner;
+    @Column(name = "name")
     private String name;
+    @Column(name = "create_date")
     private Date createDate;
+
     private List<Contact> contacts;
 
     public Group() {
