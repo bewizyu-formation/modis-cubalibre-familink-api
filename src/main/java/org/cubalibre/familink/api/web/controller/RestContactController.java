e9ee9868894965a7b87eadad171322d435fe514e
avlas <avlas@users.noreply.github.com>
2017-10-11 19:56:54 +0200

    POST contact (#30)

---
+++ src/main/java/org/cubalibre/familink/api/web/controller/RestContactController.java
@@ -0,0 +1,29 @@
+package org.cubalibre.familink.api.web.controller;
+
+import org.cubalibre.familink.api.entite.Contact;
+import org.cubalibre.familink.api.services.IContactService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contacts")
+public class RestContactController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestContactController.class.getName());
+
+    @Autowired
+    @Qualifier("contactService")
+    private IContactService contactService;
+
+    @RequestMapping(path = "/", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
+    public Contact createContact(@RequestBody Contact newContact) {
+        return contactService.create(newContact);
+    }
+
+}
