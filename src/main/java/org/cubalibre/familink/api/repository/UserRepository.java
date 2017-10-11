911729eac6220a509e96e77aed98b537664f7cfc
KurayamiNoDaiTenshi <angelo67170@gmail.com>
2017-10-11 20:33:26 +0200

    Post user (#31)
    * add DB diagram
    
    * create Post User route
    
    * User Post

---
+++ src/main/java/org/cubalibre/familink/api/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.cubalibre.familink.api.repository;
+
+import org.cubalibre.familink.api.entite.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Classe de type Repository (Spring Data)
+ *
+ * @author avl
+ */
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	
+}
---
+++ src/main/java/org/cubalibre/familink/api/web/controller/RestUserController.java
@@ -0,0 +1,28 @@
+package org.cubalibre.familink.api.web.controller;
+
+import org.cubalibre.familink.api.entite.User;
+import org.cubalibre.familink.api.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class RestUserController {
+
+
+    @Autowired
+    @Qualifier("userService")
+    private IUserService userService;
+
+    @RequestMapping(path = "/", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
+    public User createUser(@RequestBody User newUser) {
+
+        return userService.create(newUser);
+    }
+
+
+}
