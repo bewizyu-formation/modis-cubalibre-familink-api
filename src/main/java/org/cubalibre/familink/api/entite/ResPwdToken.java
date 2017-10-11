f87eb4a39351bbba32f129dfbe6fa72fb1b63857
avlas <avlas@gmail.com>
2017-10-11 18:49:22 +0200

    reset pwd entitie

---
+++ src/main/java/org/cubalibre/familink/api/entite/ResPwdToken.java
@@ -0,0 +1,61 @@
+package org.cubalibre.familink.api.entite;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "res_pwd_token")
+public class ResPwdToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_token")
+    private Integer id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "is_used")
+    private boolean isUsed;
+
+    /**
+     * Constuctor vide
+     *
+     * @return
+     */
+    public ResPwdToken() {
+
+    }
+
+    /**
+     * Constructor
+     *
+     * @param id
+     * @param token
+     * @param isUsed
+     */
+    public ResPwdToken(Integer id, String token, boolean isUsed) {
+        this.id = id;
+        this.token = token;
+        this.isUsed = isUsed;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
+}
