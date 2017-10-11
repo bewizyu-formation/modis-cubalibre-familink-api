ab90ea881069a2bf592b80174b7b4ef6f4618bf0
avlas <avlas@gmail.com>
2017-10-11 18:19:38 +0200

    User & Contact entities

---
+++ src/main/java/org/cubalibre/familink/api/entite/User.java
@@ -0,0 +1,69 @@
+package org.cubalibre.familink.api.entite;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private int id;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "contact_id")
+    private Contact contact;
+
+    /**
+     * Constructor
+     *
+     * @param id
+     * @param mail
+     * @param password
+     * @param contact
+     */
+    public User(int id, String mail, String password, Contact contact) {
+        this.id = id;
+        this.mail = mail;
+        this.password = password;
+        this.contact = contact;
+    }
+
+    /**
+     * Contructor vide
+     */
+    public User() {
+    }
+
+    /**
+     * GETTERS & SETTERS
+     *
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+}
