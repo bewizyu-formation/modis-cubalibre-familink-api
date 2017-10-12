035adac1dc0a24807b12dae369ccce6c8505b335
avlas <avlas@gmail.com>
2017-10-12 14:39:45 +0200

    refactor Contact creation

--- src/main/java/org/cubalibre/familink/api/services/impl/ContactService.java
+++ src/main/java/org/cubalibre/familink/api/services/impl/ContactService.java
@@ -1,7 +1,6 @@
 package org.cubalibre.familink.api.services.impl;
 
 import org.cubalibre.familink.api.entite.Contact;
-import org.cubalibre.familink.api.entite.Profil;
 import org.cubalibre.familink.api.repository.ContactRepository;
 import org.cubalibre.familink.api.services.IContactService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +14,6 @@ public class ContactService implements IContactService {
     @Autowired
     private ContactRepository contactRepository;
 
-    @Autowired
-    private ProfilService profilService;
-
     @Override
     public List<Contact> getContacts() {
 
@@ -26,9 +22,6 @@ public class ContactService implements IContactService {
 
     @Override
     public Contact create(Contact newContact) {
-
-        Profil contactProfil = profilService.getProfilById(newContact.getProfil().getId());
-        newContact.setProfil(contactProfil);
 
         return contactRepository.save(newContact);
     }
--- src/main/java/org/cubalibre/familink/api/web/controller/RestContactController.java
+++ src/main/java/org/cubalibre/familink/api/web/controller/RestContactController.java
@@ -1,7 +1,9 @@
 package org.cubalibre.familink.api.web.controller;
 
 import org.cubalibre.familink.api.entite.Contact;
+import org.cubalibre.familink.api.entite.Profil;
 import org.cubalibre.familink.api.services.IContactService;
+import org.cubalibre.familink.api.services.impl.ProfilService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +23,18 @@ public class RestContactController {
     @Qualifier("contactService")
     private IContactService contactService;
 
+    @Autowired
+    @Qualifier("profilService")
+    private ProfilService profilService;
+
     // ********** CREATE CONTACT ********** //
     @RequestMapping(path = "/", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
     public Contact createContact(@RequestBody Contact newContact) {
+
+        Profil profilNewContact = profilService.getProfilById(newContact.getProfil().getId());
+        if (profilNewContact != null) {
+            newContact.setProfil(profilNewContact);
+        }
 
         return contactService.create(newContact);
     }
