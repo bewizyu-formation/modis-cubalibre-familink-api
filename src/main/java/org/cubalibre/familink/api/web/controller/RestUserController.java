e087c007843db5fc6d50f7d8515d1a77528f8c0c
avlas <avlas@users.noreply.github.com>
2017-10-12 15:19:36 +0200

    refactor Contact Update (#42)

--- src/main/java/org/cubalibre/familink/api/web/controller/RestUserController.java
+++ src/main/java/org/cubalibre/familink/api/web/controller/RestUserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class RestUserController {
 
 
