7439f70d8b7abc553328ffa52d10867451aa0b1f
Angelo Basso <angelo67170@gmail.com>
2017-10-18 15:28:51 +0200

    add user to auth

--- src/main/java/org/cubalibre/familink/api/web/controller/RestAuthController.java
+++ src/main/java/org/cubalibre/familink/api/web/controller/RestAuthController.java
@@ -26,13 +26,36 @@ public class RestAuthController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         JSONObject body = new JSONObject();
+        JSONObject jsonUser = new JSONObject();
+        JSONObject jsonContact = new JSONObject();
+        JSONObject jsonProfil = new JSONObject();
         if (existingUser != null) {
             try {
                 token = TokenUtils.generateToken(existingUser.getId());
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
+
             body.put("token", token);
+            jsonUser.put("id", existingUser.getId());
+            jsonUser.put("mail", existingUser.getMail());
+            jsonContact.put("id", existingUser.getContact().getId());
+            jsonContact.put("lastname", existingUser.getContact().getLastName());
+            jsonContact.put("firstname", existingUser.getContact().getFirstName());
+            jsonContact.put("phone", existingUser.getContact().getPhone());
+
+            jsonProfil = new JSONObject();
+            jsonProfil.put("id", existingUser.getContact().getProfil().getId());
+            jsonProfil.put("type", existingUser.getContact().getProfil().getType());
+            jsonProfil.put("color", existingUser.getContact().getProfil().getColor());
+            jsonContact.put("profil", jsonProfil);
+
+            jsonContact.put("address", existingUser.getContact().getAddress());
+            jsonContact.put("zipcode", existingUser.getContact().getZipcode());
+            jsonContact.put("city", existingUser.getContact().getCity());
+            jsonContact.put("gravatar", existingUser.getContact().getGravatar());
+            jsonUser.put("contact", jsonContact);
+            body.put("user", jsonUser);
         }
         return new ResponseEntity<>(body.toString(), headers, HttpStatus.ACCEPTED);
     }
